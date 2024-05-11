ae622b3d3f38619f82285479f1a37de2924e5523
Ghhjknnh <dnjsrn333@naver.com>
2024-05-11 22:37:02 +0900

    first commit

--- src/Daiso.java
+++ src/Daiso.java
@@ -5,8 +5,7 @@ public class Daiso {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DBconnect test = new DBconnect();
-		test.connectDB();
+		
 		Function 기능 = new Function();
 		Scanner sc = new Scanner(System.in);
 		int select;
@@ -24,22 +23,47 @@ public class Daiso {
 		    
 		
 		if(loginSuccess==true) {
-			while(true) {
-				System.out.println("1.매장 물품 리스트\n2.매장 물품 검색\n3.매장 입출고 관리\n4.끝내기");
-				select = sc.nextInt();
-				switch(select) {
-				case 1 : {기능.물품리스트(); continue;}
-				case 2 : 
-				case 3 : System.out.print("1.입고\n2.출고");
-							select = sc.nextInt();
-							switch(select){
-							case 1: {기능.입고(); continue;}
-							case 2: {기능.출고(); continue;}
-							}
-							
-				case 4 : {System.out.print("종료합니다"); break;}
-				}
-			}
+			 while (true) {
+		            System.out.println("1. 매장 물품 리스트\n2. 신규 물품 등록\n3. 매장 입출고 관리\n4. 끝내기");
+		            System.out.print("메뉴를 선택하세요: ");
+		            select = sc.nextInt();
+		            switch (select) {
+		                case 1:
+		                    System.out.println("매장 물품 리스트를 출력합니다.");
+		                    기능.물품리스트();
+		                    break;
+		                case 2:
+		                    System.out.println("신규 물품 등록");
+		                    기능.신규물품();
+		                    break;
+		                case 3:
+		                    System.out.println("매장 입출고를 관리합니다.");
+		                    System.out.println("1. 입고\n2. 출고");
+		                    int inoutSelect = sc.nextInt();
+		                    switch (inoutSelect) {
+		                        case 1:
+		                            System.out.println("물품을 입고합니다.");
+		                            기능.입고();
+		                            break;
+		                        case 2:
+		                            System.out.println("물품을 출고합니다.");
+		                            기능.출고();
+		                            break;
+		                        default:
+		                            System.out.println("잘못된 선택입니다.");
+		                            break;
+		                    }
+		                    break;
+		                case 4:
+		                    System.out.println("프로그램을 종료합니다.");
+		                    System.exit(0);
+		                    break;
+		                default:
+		                    System.out.println("잘못된 선택입니다.");
+		                    break;
+		            }
+		        }
+		    
 		}else{
 			System.out.print("로그인 실패");
 		}
--- src/Function.java
+++ src/Function.java
@@ -26,7 +26,10 @@ public class Function {
 				int pcount = rs.getInt("pcount");
 				String	ploction = rs.getString("plocation");
 				
-				System.out.println("물품번호:" + pid + "\t"+ "물품개수:" + pcount + "\t" +"물품:"+ pName + "\t" +"가격:" + pprice + "\t" + "위치:"+ ploction);
+				System.out.println("물품:" + pName + "\t" +
+		                   "물품개수:" + pcount + "\t" +
+		                   "가격:" + pprice + "\t" +
+		                   "위치:" + ploction);
 			}
 			
 			conn.close();
@@ -39,10 +42,10 @@ public class Function {
 	
 	
 	
-	public void 입고() {
+	public void 신규물품() {
 		
 		
-		System.out.println("입고시킬 제품의 이름과 가격, 위치, 수량을 입력해주세요");
+		System.out.println("등록할 제품의 이름, 수량, 위치, 가격을 입력해주세요");
 		String name = sc.next();
 		int	   price = sc.nextInt();
 		String location = sc.next();
@@ -54,10 +57,10 @@ public class Function {
 			stmt = conn.createStatement();
 			
 			int i = stmt.executeUpdate("INSERT INTO product(pname, pprice, plocation, pcount) VALUES ('" + 
-	                name + "', " + price + ", '" + location + "', " + pcount + ");");
+	                name + "', " + pcount + ", '" + location + "', " + price + ");");
 			
 			if (i == 1) 
-				System.out.println("상품이 입고 되었습니다");
+				System.out.println("상품이 등록 되었습니다");
 			else
 				System.out.println("데이터 입력 오류입니다.");
 			
@@ -69,11 +72,42 @@ public class Function {
 		}
 		
 	}
+	
+	public void 입고() {
+		
+		
+		System.out.println("입고시킬 제품의 이름과 수량을 입력해주세요");
+		String	pname = sc.next();
+		int pcount = sc.nextInt();
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");		
+			conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/daiso", "root","rlsmr123");
+															
+			stmt = conn.createStatement();
+			
+			int i = stmt.executeUpdate("UPDATE product SET pcount = pcount + " + pcount + " WHERE pname = '" + pname + "'");
+			
+			if (i == 1) 
+				System.out.println("1개의 상품이 입고 되었습니다");
+			else
+				System.out.println("데이터 입력 오류입니다.");
+			
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC 드라이버 로드 에러");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+		}
+		
+	}
+	
 	public void 출고() {
 			
 			
-			System.out.println("출고시킬 제품의 번호를 입력해주세요");
-			int	   pid = sc.nextInt();
+			System.out.println("출고시킬 제품의 이름과 수량을 입력해주세요");
+			String	pname = sc.next();
+			int pcount = sc.nextInt();
 			
 			try {
 				Class.forName("org.mariadb.jdbc.Driver");		
@@ -81,7 +115,7 @@ public class Function {
 																
 				stmt = conn.createStatement();
 				
-				int i = stmt.executeUpdate("DELETE FROM product WHERE pid =" + pid);
+				int i = stmt.executeUpdate("UPDATE product SET pcount = pcount - " + pcount + " WHERE pname = '" + pname + "'");
 				
 				if (i == 1) 
 					System.out.println("1개의 상품이 출고 되었습니다");
@@ -96,6 +130,8 @@ public class Function {
 			}
 			
 		}
+	
+
   
 }
 
